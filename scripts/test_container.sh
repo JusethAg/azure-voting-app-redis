71799e78b963a010aad2d4875228d1e9c03b004f
JusethAg <mauagueda@gmail.com>
2020-08-09 15:58:57 -0500

    Change expected status on test

--- scripts/test_container.sh
+++ scripts/test_container.sh
@@ -7,7 +7,7 @@ while [ "$count" != 5 -a "$started" != "true" ]; do
   
   status_code=$(curl -LI http://localhost:8081 -o /dev/null -w '%{http_code}\n' -s)
 
-  if [ "$status_code" -eq 200 ]; then 
+  if [ "$status_code" -eq 500 ]; then 
     started=true
   else
     sleep 1
